8dc759afa55cbee8e0a6c6d03d6911ddca5141c4
Steve Tyler <steve.tyler@centrica.com>
2019-08-23 18:15:20 +0100

    set state on square component

--- src/Square.js
+++ src/Square.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react'
 
 class Square extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null,
+    };
+  }
   render() {
     return (
-      <button className="square" onClick={ function() { alert('clicked');} }>
-        {this.props.value}
+      <button
+        className="square"
+        onClick={ () => { this.setState({value: 'X'}) }}>
+        {this.state.value}
       </button>
     );
   }
