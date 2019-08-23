6e73f82927b767125a5fcfb7ed34f7998c42aa6a
Steve Tyler <steve.tyler@centrica.com>
2019-08-23 20:57:32 +0100

    further implementation

--- src/Board.js
+++ src/Board.js
@@ -2,8 +2,34 @@ import React, { Component } from 'react'
 import Square from './Square'
 
 class Board extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+      xIsNext: true
+    }
+  }
+  /*
+    Notice how with onClick={() => alert('click')},
+    we’re passing a function as the onClick prop.
+    React will only call this function after a click.
+    Forgetting () => and writing onClick={alert('click')} is a common mistake, and would fire the alert every time the component re-renders.
+  */
   renderSquare(i) {
-    return <Square value={i} />;
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => { this.handleClick(i) }}
+      />
+    )
+  }
+  handleClick(i) {
+    const squares = this.state.squares.slice() // create copy for immutability
+    squares[i] = this.state.xIsNext ? 'X' : 'O'
+    this.setState({
+      squares,
+      xIsNext: !this.state.xIsNext
+    })
   }
 
   render() {
--- src/Square.js
+++ src/Square.js
@@ -1,21 +1,19 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-class Square extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: null,
-    };
-  }
-  render() {
-    return (
-      <button
-        className="square"
-        onClick={ () => { this.setState({value: 'X'}) }}>
-        {this.state.value}
-      </button>
-    );
-  }
+/*
+  In React, function components are a simpler way to write components that only contain a render method and don’t have their own state. Instead of defining a class which extends React.Component, we can write a function that takes props as input and returns what should be rendered. Function components are less tedious to write than classes, and many components can be expressed this way.
+*/
+function Square(props) {
+  return (
+    /*
+      The DOM <button> element’s onClick attribute has a special meaning to React because it is a built-in component. For custom components like Square, the naming is up to you. We could give any name to the Square’s onClick prop or Board’s handleClick method, and the code would work the same. In React, it’s conventional to use on[Event] names for props which represent events and handle[Event] for the methods which handle the events.
+
+      When we modified the Square to be a function component, we also changed onClick={() => this.props.onClick()} to a shorter onClick={props.onClick} (note the lack of parentheses on both sides).
+    */
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
 }
 
 export default Square
