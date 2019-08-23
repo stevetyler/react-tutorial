9c802f3dd8179cceb4a69ccfe22ea0828b853109
Steve Tyler <steve.tyler@centrica.com>
2019-08-23 20:57:32 +0100

    further implementation

--- src/Board.js
+++ src/Board.js
@@ -2,12 +2,48 @@ import React, { Component } from 'react'
 import Square from './Square'
 
 class Board extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+      xIsNext: true
+    }
+  }
+  /*
+    Notice how with onClick={() => alert('click')},
+    we’re passing a function as the onClick prop.
+    React will only call this function after a click.
+    Forgetting () => and writing onClick={alert('click')} is a common mistake, and would fire the alert every time the component re-renders.
+  */
   renderSquare(i) {
-    return <Square value={i} />;
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => { this.handleClick(i) }}
+      />
+    )
+  }
+  handleClick(i) {
+    const squares = this.state.squares.slice() // create copy for immutability
+    // Ignore a click if someone has won the game or if a Square is already filled:
+    if (calculateWinner(squares) || squares[i]) {
+      return
+    }
+    squares[i] = this.state.xIsNext ? 'X' : 'O'
+    this.setState({
+      squares,
+      xIsNext: !this.state.xIsNext
+    })
   }
 
   render() {
-    const status = 'Next player: X';
+    const winner = calculateWinner(this.state.squares)
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
 
     return (
       <div>
@@ -33,3 +69,23 @@ class Board extends Component {
 }
 
 export default Board
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
