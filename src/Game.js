9b2b86f9458ffafaded04d41e22f542d3de93ae3
Steve Tyler <steve.tyler@centrica.com>
2019-08-27 20:08:54 +0100

    updated

--- src/Game.js
+++ src/Game.js
@@ -8,27 +8,52 @@ class Game extends Component {
       history: [{
         squares: Array(9).fill(null)
       }],
-      xIsNext: true
+      xIsNext: true,
+      stepNumber: 0
     }
   }
   handleClick(i) {
-    const history = this.state.history
+    /* This ensures that if we “go back in time” and then make a new move from that point,
+      we throw away all the “future” history that would now become incorrect. */
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const current = history[history.length - 1]
     const squares = current.squares.slice() // create copy for immutability
+
+    console.log(history.length)
     // Ignore a click if someone has won the game or if a Square is already filled:
     if (calculateWinner(squares) || squares[i]) {
       return
     }
     squares[i] = this.state.xIsNext ? 'X' : 'O'
     this.setState({
-      history: history.concat(squares),
+      history: history.concat([{
+        squares
+      }]),
+      stepNumber: history.length,
       xIsNext: !this.state.xIsNext
+    })
+  }
+
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: step % 2 === 0
     })
   }
   render() {
     const history = this.state.history;
-    const current = history[history.length - 1]
+    const current = history[this.state.stepNumber]
     const winner = calculateWinner(current.squares)
+
+    const moves = history.map((step, move) => {
+      const desc = move ? `Go to move #${move}` : 'Go to game start'
+      return (
+        <li key={move}>
+          <button onClick={() => this.jumpTo(move)}>{desc}</button>
+        </li>
+      )
+    })
+
     let status;
     if (winner) {
       status = 'Winner: ' + winner;
@@ -45,14 +70,13 @@ class Game extends Component {
         </div>
         <div className="game-info">
           <div>{status}</div>
-          <ol>{/* TODO */}</ol>
+          <ol>{moves}</ol>
         </div>
       </div>
     );
   }
 }
 
-export default Game
 
 function calculateWinner(squares) {
   const lines = [
@@ -73,3 +97,6 @@ function calculateWinner(squares) {
   }
   return null;
 }
+
+
+export default Game
